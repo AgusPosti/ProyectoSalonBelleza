f2ce30120347c99d26f0940c58bc203263e64a7d
Mateomontoya <mateomontoya414@gmail.com>
2026-09-17 00:20:00 -0300

    SE AGREGO Y MODIFICO DETALLES 2

--- TurneraSalonBelleza/client/src/app/app.routes.ts
+++ TurneraSalonBelleza/client/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Inicio } from './pages/inicio/inicio';
 import { Cliente } from './pages/cliente/cliente';
 import { Admin } from './pages/admin/admin';
+import { Empleado } from './pages/empleado/empleado';
 import { ReservarTurno } from './pages/reservar-turno/reservar-turno';
 
 
@@ -10,5 +11,6 @@ export const routes: Routes = [
   { path: 'cliente', component: Cliente },
   { path: 'admin', component: Admin },
   { path: 'reservar-turno', component: ReservarTurno },
+  { path: 'empleado', component: Empleado}
 
 ];
