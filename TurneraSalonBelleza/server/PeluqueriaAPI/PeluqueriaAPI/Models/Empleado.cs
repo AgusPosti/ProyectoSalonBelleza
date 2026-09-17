2abb8822fe4fd2d366a1b1fe47e29281f78e9a07
111ware <ramirocamelinoe@gmail.com>
2026-09-17 18:19:03 -0300

    Se sube la primera parte de la DB y APIweb en c#

---
+++ TurneraSalonBelleza/server/PeluqueriaAPI/PeluqueriaAPI/Models/Empleado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeluqueriaAPI.Models;
+
+public partial class Empleado
+{
+    public int IdEmpleado { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Especialidad { get; set; } = null!;
+
+    public virtual ICollection<Turno> Turnos { get; set; } = new List<Turno>();
+}
---
+++ TurneraSalonBelleza/server/PeluqueriaAPI/PeluqueriaAPI/Models/PeluqueriaContext.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace PeluqueriaAPI.Models;
+
+public partial class PeluqueriaContext : DbContext
+{
+    public PeluqueriaContext()
+    {
+    }
+
+    public PeluqueriaContext(DbContextOptions<PeluqueriaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    public virtual DbSet<Empleado> Empleados { get; set; }
+
+    public virtual DbSet<Servicio> Servicios { get; set; }
+
+    public virtual DbSet<Turno> Turnos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.HasKey(e => e.IdCliente).HasName("PRIMARY");
+
+            entity.ToTable("clientes");
+
+            entity.HasIndex(e => e.Email, "email").IsUnique();
+
+            entity.HasIndex(e => e.Telefono, "telefono").IsUnique();
+
+            entity.Property(e => e.IdCliente).HasColumnName("id_cliente");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .HasColumnName("email");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(100)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Telefono)
+                .HasMaxLength(100)
+                .HasColumnName("telefono");
+        });
+
+        modelBuilder.Entity<Empleado>(entity =>
+        {
+            entity.HasKey(e => e.IdEmpleado).HasName("PRIMARY");
+
+            entity.ToTable("empleados");
+
+            entity.Property(e => e.IdEmpleado).HasColumnName("id_empleado");
+            entity.Property(e => e.Especialidad)
+                .HasMaxLength(100)
+                .HasColumnName("especialidad");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(100)
+                .HasColumnName("nombre");
+        });
+
+        modelBuilder.Entity<Servicio>(entity =>
+        {
+            entity.HasKey(e => e.IdServicio).HasName("PRIMARY");
+
+            entity.ToTable("servicios");
+
+            entity.Property(e => e.IdServicio).HasColumnName("id_servicio");
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(255)
+                .HasColumnName("descripcion");
+            entity.Property(e => e.Duracion).HasColumnName("duracion");
+            entity.Property(e => e.NombreServicio)
+                .HasMaxLength(120)
+                .HasColumnName("nombre_servicio");
+            entity.Property(e => e.Precio)
+                .HasPrecision(10, 2)
+                .HasColumnName("precio");
+        });
+
+        modelBuilder.Entity<Turno>(entity =>
+        {
+            entity.HasKey(e => e.IdTurno).HasName("PRIMARY");
+
+            entity.ToTable("turnos");
+
+            entity.HasIndex(e => e.IdCliente, "fk_turno_cliente");
+
+            entity.HasIndex(e => e.IdEmpleado, "fk_turno_empleado");
+
+            entity.Property(e => e.IdTurno).HasColumnName("id_turno");
+            entity.Property(e => e.Estado)
+                .HasMaxLength(30)
+                .HasColumnName("estado");
+            entity.Property(e => e.FechaHora)
+                .HasColumnType("datetime")
+                .HasColumnName("fecha_hora");
+            entity.Property(e => e.IdCliente).HasColumnName("id_cliente");
+            entity.Property(e => e.IdEmpleado).HasColumnName("id_empleado");
+
+            entity.HasOne(d => d.IdClienteNavigation).WithMany(p => p.Turnos)
+                .HasForeignKey(d => d.IdCliente)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_turno_cliente");
+
+            entity.HasOne(d => d.IdEmpleadoNavigation).WithMany(p => p.Turnos)
+                .HasForeignKey(d => d.IdEmpleado)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_turno_empleado");
+
+            entity.HasMany(d => d.IdServicios).WithMany(p => p.IdTurnos)
+                .UsingEntity<Dictionary<string, object>>(
+                    "TurnosServicio",
+                    r => r.HasOne<Servicio>().WithMany()
+                        .HasForeignKey("IdServicio")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("fk_ts_servicio"),
+                    l => l.HasOne<Turno>().WithMany()
+                        .HasForeignKey("IdTurno")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("fk_ts_turno"),
+                    j =>
+                    {
+                        j.HasKey("IdTurno", "IdServicio")
+                            .HasName("PRIMARY")
+                            .HasAnnotation("MySql:IndexPrefixLength", new[] { 0, 0 });
+                        j.ToTable("turnos_servicios");
+                        j.HasIndex(new[] { "IdServicio" }, "fk_ts_servicio");
+                        j.IndexerProperty<int>("IdTurno").HasColumnName("id_turno");
+                        j.IndexerProperty<int>("IdServicio").HasColumnName("id_servicio");
+                    });
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
